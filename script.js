9617f353c36fb5bc940371518852b623e5fc6353
Matias Benites <matiasbenites665@gmail.com>
2022-07-24 00:05:16 -0300

    First functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('JS is loaded');
+//text
+const dataInput = document.querySelector("#data");
+console.log(dataInput.value);
+
+//colors from here
+const mainColorPicker = document.querySelector('#main-color');
+const backgroundColorPicker = document.querySelector('#bg-color');
+
+const updateColor = () => {
+    const value = e.target.value;
+}
+
+const addColorPickerEventListener = () => { 
+    mainColorPicker.addEventListener('change', updateColor);
+    backgroundColorPicker.addEventListener('change', updateBackgroundColor) 
+}
+
+addColorPickerEventListener();
